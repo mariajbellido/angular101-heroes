012b618578277d88d36d43c8601bd9376e64dd00
mariajbellido <mariajbellido@gmail.com>
2021-10-20 10:39:34 +0200

    Tarea con inputs y módulos

--- src/app/dbz/main-page/main-page.component.ts
+++ src/app/dbz/main-page/main-page.component.ts
@@ -23,33 +23,8 @@ export class MainPageComponent{
       poder: 7500
     }
 
-  ]
+  ];
   
-  nuevo: Personaje = {
-    nombre: '',
-    poder: 0
-  }
-
-  cambiarNombre( event: any) {
-    console.log(event.target.value)
-  }
-
-  agregar()  {
-
-    if (this.nuevo.nombre.trim().length === 0) {
-      return;
-    }
-
-    console.log(this.nuevo);
-
-    this.personajes.push(this.nuevo);
-
-    this.nuevo = {
-      nombre: '',
-      poder: 0
-    }
-
-  }
-
+ 
   
 }
